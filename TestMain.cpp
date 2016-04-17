24a85223c6c7f2940f4383f5bcb32102dd5e6775
DaveWeathers <beruay@gmail.com>
2016-04-16 21:45:16 -0500

    edited

--- TestMain.cpp
+++ TestMain.cpp
@@ -9,37 +9,38 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include "WorkingSet.cpp"
+//#include "ReferenceSet.cpp"
 #include "ReplacementStrategies.cpp"
-#include "ReferenceSet.cpp"
 
 using namespace std;
 int RESIDENT_SET_SIZE;
-//int NUMBER_OF_REFERENCES;
+//int NUMBER_OF_PAGES;
 int const BREAK_POINT = 40;
 
 int LRU(int arr[]){
-	list<int> residentSet;
-	int pageFault = 0;
-	for(int i = 0; i < NUMBER_OF_REFERENCES; i ++){
-		if(residentSet.size() < RESIDENT_SET_SIZE && !(find(residentSet.begin(), residentSet.end(), arr[i]) != residentSet.end())){
-			pageFault++;
-			residentSet.push_back(arr[i]);
+		list<int> residentSet;
+		int pageFault = 0;
+		for(int i = 0; i < NUMBER_OF_PAGES; i ++){
+			if(residentSet.size() < RESIDENT_SET_SIZE && !(find(residentSet.begin(), residentSet.end(), arr[i]) != residentSet.end())){
+				pageFault++;
+				residentSet.push_back(arr[i]);
+			}
+			else if(!(find(residentSet.begin(), residentSet.end(), arr[i]) != residentSet.end())){
+				pageFault++;
+				residentSet.pop_front();
+				residentSet.push_back(arr[i]);
+			}
+			else {
+				residentSet.remove(arr[i]);
+				residentSet.push_back(arr[i]);
+			}
 		}
-		else if(!(find(residentSet.begin(), residentSet.end(), arr[i]) != residentSet.end())){
-			pageFault++;
-			residentSet.pop_front();
-			residentSet.push_back(arr[i]);
-		}
-		else {
-			residentSet.remove(arr[i]);
-			residentSet.push_back(arr[i]);
-		}
+		return pageFault;
 	}
-	return pageFault;
-}
 
 int FutureGazing(int i, list<int> set, int* arr){
-	for (int j = i; j < NUMBER_OF_REFERENCES; j++){
+	for (int j = i; j < NUMBER_OF_PAGES; j++){
 		if(set.size() == 1){
 			return set.front();
 		}
@@ -54,21 +55,21 @@ int Optimal(int arr[]){
 	list<int> residentSet;
 	int pageFault = 0;
 	list<int> oracleSet;
-	for(int i = 0; i < NUMBER_OF_REFERENCES; i ++){
-		if(residentSet.size() < RESIDENT_SET_SIZE && !(find(residentSet.begin(), residentSet.end(), arr[i]) != residentSet.end())){
-			pageFault++;
-			residentSet.push_back(arr[i]);
-		}
-		else if(!(find(residentSet.begin(), residentSet.end(), arr[i]) != residentSet.end())){
-			pageFault++;
-			oracleSet = residentSet;
-			residentSet.remove(FutureGazing(i,oracleSet, arr));
-			residentSet.push_back(arr[i]);
-		}
+	for(int i = 0; i < NUMBER_OF_PAGES; i ++){
+				if(residentSet.size() < RESIDENT_SET_SIZE && !(find(residentSet.begin(), residentSet.end(), arr[i]) != residentSet.end())){
+					pageFault++;
+					residentSet.push_back(arr[i]);
+				}
+				else if(!(find(residentSet.begin(), residentSet.end(), arr[i]) != residentSet.end())){
+					pageFault++;
+					oracleSet = residentSet;
+					residentSet.remove(FutureGazing(i,oracleSet, arr));
+					residentSet.push_back(arr[i]);
+				}
 
-	}
-	return pageFault;
-}
+			}
+			return pageFault;
+		}
 
 int leastOU(int count[], list<int> currentSet){
 	int replace = 10000000;
@@ -86,31 +87,31 @@ int leastOU(int count[], list<int> currentSet){
 }
 
 void resetMax(int count[]){
-	for (int i = 0; i < NUMBER_OF_PAGES; i++){
+	for (int i = 0; i < DIFFERENT_PAGES; i++){
 		count[i] = count[i]/2;
 	}
 }
 
 int lou(int arr[] ){
-	int count[NUMBER_OF_PAGES];
+	int count[DIFFERENT_PAGES];
 	int pageFault = 0;
 	list<int> residentSet;
 
-	for (int i = 0; i < NUMBER_OF_REFERENCES; i++){
-		if(residentSet.size()< RESIDENT_SET_SIZE) {
-			if  (!(find(residentSet.begin(), residentSet.end(), arr[i]) != residentSet.end())){
+	for (int i = 0; i < NUMBER_OF_PAGES; i++){
+			if(residentSet.size()< RESIDENT_SET_SIZE) {
+				if  (!(find(residentSet.begin(), residentSet.end(), arr[i]) != residentSet.end())){
 				pageFault++;
 				residentSet.push_back(arr[i]);
 				count[i] = 1;
-			}
-			else {
-				count[arr[i]] = count[arr[i]] + 1;
-				if(count[arr[i]]>= BREAK_POINT){
-					resetMax(count);
 				}
-			}
-		}else {
-			if(!(find(residentSet.begin(), residentSet.end(), arr[i]) != residentSet.end())){
+				else {
+					count[arr[i]] = count[arr[i]] + 1;
+					if(count[arr[i]]>= BREAK_POINT){
+						resetMax(count);
+					}
+				}
+			}else {
+				if(!(find(residentSet.begin(), residentSet.end(), arr[i]) != residentSet.end())){
 				int temp = leastOU(count, residentSet);
 				residentSet.remove(temp);
 				residentSet.push_back(arr[i]);
@@ -122,17 +123,17 @@ int lou(int arr[] ){
 
 
 
-			}else{
-				count[arr[i]] = count[arr[i]] + 1;
-				if(count[arr[i]]>= BREAK_POINT){
-					resetMax(count);
+				}else{
+					count[arr[i]] = count[arr[i]] + 1;
+					if(count[arr[i]]>= BREAK_POINT){
+						resetMax(count);
+					}
 				}
-			}
 
 
 		}
 	}
-	return pageFault;
+return pageFault;
 }
 
 
@@ -140,31 +141,30 @@ int lou(int arr[] ){
 
 
 int main()
-{
-	GoodProgram a;
-	BadProgram b;
-	WellBehavedModulesProgram c;
-	int* good = a.buildSet();
-	int* bad = b.buildSet();
-	int* realistic = c.buildSet();
-	printf("LRU-Good, LRU-Bad, LRU-Well Behaved, OPTIMAL-Good, OPTIMAL-Bad, OPTIMAL-Well Behaved, LOU-Good, LOU-Bad, LOU-Well Behaved\n");
-	for(int i = 1; i <= 50; i++){
-		RESIDENT_SET_SIZE = i;
+	{
+			GoodProgram a;
+			BadProgram b;
+			WellBehavedModulesProgram c;
+			int* good = a.buildSet();
+			int* bad = b.buildSet();
+			int* wellBehaved = c.buildSet();
 
+			printf("LRU-Good, LRU-Bad, LRU-Well Behaved, OPTIMAL-Good, OPTIMAL-Bad, OPTIMAL-Well Behaved, LOU-Good, LOU-Bad, LOU-Well Behaved\n");
+		for(int i = 1; i <= 50; i++){
+			RESIDENT_SET_SIZE = i;
 
-		printf("%d",i);
-		printf("%d,", LRU(good));
-		printf("%d,", LRU(bad));
-		printf("%d,", LRU(realistic));
-		printf("%d,", Optimal(good));
-		printf("%d,", Optimal(bad));
-		printf("%d,", Optimal(realistic));
-		printf("%d,", lou(good));
-		printf("%d", lou(bad));
-		printf("%d\n", lou(realistic));
+			fflush(stdout);
+			printf("%d,",i);
+			printf("%d,", LRU(good));
+			printf("%d,", LRU(bad));
+			printf("%d,", LRU(wellBehaved));
+			printf("%d,", Optimal(good));
+			printf("%d,", Optimal(bad));
+			printf("%d,", Optimal(wellBehaved));
+			printf("%d,", lou(good));
+			printf("%d,", lou(bad));
+			printf("%d\n", lou(wellBehaved));
 
-	}
+			}
 	return 0;
 	}
-
-
--- WorkingSet.cpp
+++ WorkingSet.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdlib.h>
 #include <ctime>
+#include <stdio.h>
+using namespace std;
 //Base WorkingSet
 /*
  * WorkingSet.cpp
@@ -9,15 +11,49 @@
  *  Created on: Mar 16, 2016
  *      Author: David Weathers
  */
-int const WORK_NUMBER_OF_REFERENCES = 10000;
+int const NUMBER_OF_PAGES = 10000;
+int const DIFFERENT_PAGES = 100;
 class WorkingSet
 {
 
 	public:
 	//virtual for interface framework
 	virtual int* buildSet()=0;
-	int workingSet[WORK_NUMBER_OF_REFERENCES];
+	int workingSet[NUMBER_OF_PAGES];
+	int workingSetCallCount[DIFFERENT_PAGES];
 	virtual ~WorkingSet(){};
+	void printPageCallArray(){
+			int i, max = 0, nextMax, topTenCount=0, tenpercent = DIFFERENT_PAGES /10;
+
+			for(i = 0; i< DIFFERENT_PAGES; i++){
+				if(max < workingSetCallCount[i]) {
+					nextMax = max;
+					max = workingSetCallCount[i];
+				}
+			}
+			while(max >= 0){
+
+				for( i = 0; i< DIFFERENT_PAGES; i++){
+					if(workingSetCallCount[i]== max){
+
+						if(tenpercent >0){
+							printf("[%d] called: %d\n", i, workingSetCallCount[i]);
+							topTenCount += workingSetCallCount[i];
+							--tenpercent;
+						}
+					}else{
+						if(workingSetCallCount[i]>nextMax && workingSetCallCount[i]<max){
+							nextMax = workingSetCallCount[i];
+						}
+					}
+
+				}
+				max = nextMax;
+				nextMax = -1;
+			}
+			printf("Percent of calls to top 10%% %2.2f", (double)topTenCount/(NUMBER_OF_PAGES/100.0));
+			cout << endl;
+		}
 };
 
 
@@ -29,9 +65,10 @@ public:
 	int* buildSet()
 		{
 		srand(time(0));
-		for (int i = 0; i < WORK_NUMBER_OF_REFERENCES; i++)
+		for (int i = 0; i < NUMBER_OF_PAGES; i++)
 			{
-			if(i%10 == 0)
+			double scatterer = rand() % 100;
+			if(scatterer > 89)
 				{
 				workingSet[i] = (rand() % 90 + 10);
 				}
@@ -53,9 +90,9 @@ public:
 	int* buildSet()
 		{
 		srand(time(0));
-		for (int i = 0; i < WORK_NUMBER_OF_REFERENCES; i++)
+		for (int i = 0; i < NUMBER_OF_PAGES; i++)
 			{
-			workingSet[i] = (rand()% 100);
+			workingSet[i] = (rand() % 100);
 			
 			}
 		return workingSet;
@@ -63,5 +100,85 @@ public:
 	~BadProgram(){};
 
 };
+
+class WellBehavedModulesProgram: public WorkingSet{
+public:
+	int* buildSet(){
+		int i, module = 0, temp;
+
+		int switchChance[] = {50,70, 95,97};
+		for(i = 0; i<DIFFERENT_PAGES;i++){ workingSetCallCount[i] = 0;}
+
+
+		double r, modUpperEndSize= 9.0;
+//		struct drand48_data localBuffer;
+//		srand48_r(time(NULL) +  (5002) + 1, &localBuffer);
+
+		srand(time(0));
+		workingSet[0]=0;
+		workingSetCallCount[0]=1;
+
+
+		//Module 1 pages 0- 9
+		//Module 2 pages 10 - 24
+		//Module 3 pages 25 - 49
+		//Module 4 pages 50 - 99
+		int modStart= 0, modSize =10;
+
+		for(i  = 1; i< NUMBER_OF_PAGES; i++) {
+			r  = rand() % 100;//drand48_r(&localBuffer, &r);
+			if(r > switchChance[module]){
+				//printf("**SWITCH: %1.2f > %f from module %d\n", r, switchChance[module], module);
+				r  = rand() % 4;//drand48_r(&localBuffer, &r);
+				module = (int)(r);//note right now equal chance can be improved..
+			}
+			switch (module){
+				case 0 :
+					modStart = 0;
+					modUpperEndSize = 9.0;
+					modSize = 10;
+					break;
+				case 1:
+					modStart = 10;
+					modUpperEndSize = 18.0;
+					modSize = 20;
+					break;
+				case 2:
+					modStart = 30;
+					modUpperEndSize = 27.0;
+					modSize = 30;
+					break;
+				case 3:
+					modStart = 50;
+					modUpperEndSize = 36.0;
+					modSize = 40;
+					break;
+			}
+
+			r  = rand() % 10;//drand48_r(&localBuffer, &r);
+			//cout<<r << endl;
+			//cout << modStart <<endl;
+			if(r< 9) {
+				r  = rand() % (modSize/10);
+				temp = ((int)((r * (10 / 9)) * ((modSize/10.0))) + modStart);
+			}else{
+				r  = rand() % (int)modUpperEndSize;
+				temp = r + (modSize/10.0) + modStart;
+			}
+			if( temp != workingSet[i-1]){
+				workingSet[i]=temp;
+//				printf("Calling Page %d: %d time\n", temp, workingSet[temp] +1);
+				int temp2 = workingSetCallCount[temp];
+				temp2 = temp2 +1;
+				workingSetCallCount[temp] = temp2;
+			}else{
+				--i;
+			}
+		}
+		return workingSet;
+	}
+	~WellBehavedModulesProgram(){};
+};
+
 
 
